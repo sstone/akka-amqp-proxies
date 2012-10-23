6839c64ba168cc6906c65b11ec4a222b841dd08c
pm47 <spam4420@gmail.com>
2012-10-23 16:26:36 +0200

    added generic snappy serializer and associated tests

---
+++ src/main/scala/com/aphelia/amqp/proxy/Serializers.scala
@@ -0,0 +1,19 @@
+package com.aphelia.amqp.proxy
+
+import serializers._
+import akka.serialization.Serializer
+
+object Serializers {
+
+  private val map = Map(
+    "json" -> JsonSerializer,
+    "protobuf" -> ProtobufSerializer,
+    "thrift" -> ThriftSerializer,
+    "snappy-json" -> SnappyJsonSerializer,
+    "snappy-protobuf" -> SnappyProtobufSerializer,
+    "snappy-thrift" -> SnappyThriftSerializer)
+
+  def nameToSerializer(name: String) = map.get(name).get
+
+  def serializerToName(serializer: Serializer) = map.map(_.swap).get(serializer).get
+}
--- src/main/scala/com/aphelia/amqp/proxy/serializers/SnappySerializer.scala
+++ src/main/scala/com/aphelia/amqp/proxy/serializers/SnappySerializer.scala
@@ -1,18 +1,17 @@
 package com.aphelia.amqp.proxy.serializers
 
-import akka.serialization.Serializer
-import com.codahale.jerkson.Json
 import org.xerial.snappy.Snappy
+import akka.serialization.Serializer
 import grizzled.slf4j.Logging
 
-object SnappyJsonSerializer extends Serializer with Logging {
+abstract class SnappySerializer(serializer: Serializer) extends Serializer with Logging {
 
-  def identifier = 987654321
+  def identifier = 4
 
   def includeManifest = true
 
   def toBinary(o: AnyRef) = {
-    val bytes = Json.generate(o).getBytes
+    val bytes = serializer.toBinary(o)
     val zipped = Snappy.compress(bytes)
     debug("zipped %dB to %dB".format(bytes.length, zipped.length))
     zipped
@@ -21,7 +20,14 @@ object SnappyJsonSerializer extends Serializer with Logging {
   def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef = {
     val unzipped = Snappy.uncompress(bytes)
     debug("unzipped %dB to %dB".format(bytes.length, unzipped.length))
-    val string = new String(unzipped)
-    Json.parse(string)(Manifest.classType(manifest.get))
+    serializer.fromBinary(unzipped, manifest)
   }
+
 }
+
+object SnappyJsonSerializer extends SnappySerializer(JsonSerializer)
+
+object SnappyProtobufSerializer extends SnappySerializer(ProtobufSerializer)
+
+object SnappyThriftSerializer extends SnappySerializer(ThriftSerializer)
+
