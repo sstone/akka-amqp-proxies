5538beae07230d4183632d873e57b0d22934ae98
Sam Stone <samuel.stone@hotmail.co.za>
2013-01-22 23:31:52 +0100

    see #11: add a test for remote calls with a JSON serializer

---
+++ src/test/scala/com/github.sstone/amqp/proxy/RemoteCallTest.scala
@@ -0,0 +1,58 @@
+package com.github.sstone.amqp.proxy
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import akka.actor.{Props, Actor, ActorSystem}
+import akka.testkit.TestKit
+import akka.pattern.ask
+import org.scalatest.{BeforeAndAfter, WordSpec}
+import org.scalatest.matchers.ShouldMatchers
+import com.rabbitmq.client.ConnectionFactory
+import serializers.JsonSerializer
+import concurrent.duration._
+import com.github.sstone.amqp.{RpcClient, Amqp, RpcServer, ConnectionOwner}
+import com.github.sstone.amqp.Amqp.{ChannelParameters, QueueParameters, ExchangeParameters}
+import concurrent.{Await, Future, ExecutionContext}
+
+@RunWith(classOf[JUnitRunner])
+class RemoteCallTest extends TestKit(ActorSystem("TestSystem")) with WordSpec with ShouldMatchers {
+  "AMQP Proxy" should {
+    "handle JSON calls" in {
+      import ExecutionContext.Implicits.global
+      val connFactory = new ConnectionFactory()
+      val conn = system.actorOf(Props(new ConnectionOwner(connFactory)), name = "conn")
+      val exchange = ExchangeParameters(name = "amq.direct", exchangeType = "", passive = true)
+      val queue = QueueParameters(name = "calculator", passive = false, autodelete = true)
+      val channelParams = Some(ChannelParameters(qos = 1))
+
+      case class AddRequest(x: Int, y: Int)
+      case class AddResponse(x: Int, y: Int, sum: Int)
+
+      // create a simple calculator actor
+      val calc = system.actorOf(Props(new Actor() {
+        def receive = {
+          case AddRequest(x, y) => sender ! AddResponse(x, y, x + y)
+        }
+      }))
+      // create an AMQP proxy server which consumes messages from the "calculator" queue and passes
+      // them to our Calculator actor
+      val server = ConnectionOwner.createActor(
+        conn,
+        Props(new RpcServer(queue, exchange, "calculator", new AmqpProxy.ProxyServer(calc), channelParams)),
+        2 second)
+
+      // create an AMQP proxy client in front of the "calculator queue"
+      val client = ConnectionOwner.createActor(conn, Props(new RpcClient()), 5 second)
+      val proxy = system.actorOf(
+        Props(new AmqpProxy.ProxyClient(client, "amq.direct", "calculator", JsonSerializer)),
+        name = "proxy")
+
+      Amqp.waitForConnection(system, server).await()
+      implicit val timeout: akka.util.Timeout = 5 seconds
+
+      val futures = for (x <- 0 to 5; y <- 0 to 5) yield (proxy ? AddRequest(x, y)).mapTo[AddResponse]
+      val result = Await.result(Future.sequence(futures), 5 seconds)
+      assert(result.filter(r => r.sum != r.x + r.y).isEmpty)
+    }
+  }
+}
